e06a583ed05a93becc758516d362ec7b74cb727e
XWRI <xiaowei4wk@gmail.com>
2020-02-06 21:38:47 -0800

    phase 4

--- preempt.c
+++ preempt.c
@@ -14,19 +14,47 @@
  * 100Hz is 100 times per second
  */
 #define HZ 100
+#
 
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+  struct sigaction signal;
+
+  sigprocmask(SIG_BLOCK, &signal.sa_mask ,NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+  struct sigaction signal;
+
+  sigprocmask(SIG_UNBLOCK, &signal.sa_mask ,NULL);
+}
+
+void handler(int signum)
+{
+  uthread_yield();
+}
+
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#Handler-Returns
+unsigned int alarm (void)
+{
+  struct itimerval old, new;
+  new.it_interval.tv_usec = 0;
+  new.it_interval.tv_sec = 0;
+  new.it_value.tv_usec = 0;
+  new.it_value.tv_sec = 0;
+  if (setitimer (ITIMER_VIRTUAL, &new, &old) < 0)
+    return 0;
+  else
+    return old.it_value.tv_sec;
 }
 
 void preempt_start(void)
 {
-	/* TODO Phase 4 */
+  struct sigaction signal;
+
+  signal.sa_handler = handler;
+  sigaction(SIGVTALRM, &signal, NULL);
+  alarm();
 }
 
